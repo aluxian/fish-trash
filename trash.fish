b4cef6ce54f3501e05a15f403fe80bbcaf46b1eb
Alexandru Rosianu <me@aluxian.com>
2020-06-06 15:05:01 +0100

    Embed AppleScript in trash.fish

--- trash.fish
+++ trash.fish
@@ -1,5 +1,11 @@
 function trash --description 'Move files to trash using Finder'
 	for arg in $argv
-		osascript trash.scpt (realpath $arg)
+		osascript -e '
+on run argv
+	tell application "Finder"
+		name of (delete ((item 1 of argv) as POSIX file))
+	end tell
+end run
+' (realpath $arg)
 	end
 end
